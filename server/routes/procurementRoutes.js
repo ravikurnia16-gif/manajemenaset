57df8be972b303e9705cc85be5cc6ef5fcb1ee0f
ravikurnia16-gif <ravikurnia16@gmail.com>
2026-02-08 23:52:14 +0700

    pengadaan 2

--- server/routes/procurementRoutes.js
+++ server/routes/procurementRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const procurementController = require('../controllers/procurementController');
-const { authenticateToken, authorizeRole } = require('../middleware/authMiddleware');
+const { verifyToken, authorizeRole } = require('../middleware/authMiddleware');
 
-router.get('/', authenticateToken, procurementController.getAllProcurements);
-router.get('/:id', authenticateToken, procurementController.getProcurementById);
-router.post('/', authenticateToken, procurementController.createProcurement);
+router.get('/', verifyToken, procurementController.getAllProcurements);
+router.get('/:id', verifyToken, procurementController.getProcurementById);
+router.post('/', verifyToken, procurementController.createProcurement);
 
 // Status Workflow
-router.put('/:id/status', authenticateToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET']), procurementController.updateStatus);
+router.put('/:id/status', verifyToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET']), procurementController.updateStatus);
 
 // Vendor Offer
-router.post('/:id/offers', authenticateToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET']), procurementController.addVendorOffer);
+router.post('/:id/offers', verifyToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET']), procurementController.addVendorOffer);
 
 // BAST & Completion
-router.post('/:id/bast', authenticateToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET']), procurementController.processBAST);
+router.post('/:id/bast', verifyToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET']), procurementController.processBAST);
 
 module.exports = router;
--- server/routes/rkbRoutes.js
+++ server/routes/rkbRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const rkbController = require('../controllers/rkbController');
-const { authenticateToken, authorizeRole } = require('../middleware/authMiddleware');
+const { verifyToken, authorizeRole } = require('../middleware/authMiddleware');
 
-router.get('/', authenticateToken, rkbController.getAllRKBs);
-router.get('/:id', authenticateToken, rkbController.getRKBById);
-router.post('/', authenticateToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET', 'ADMIN_UNIT']), rkbController.createRKB);
-router.post('/:id/items', authenticateToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET', 'ADMIN_UNIT']), rkbController.addItem);
-router.put('/:id/status', authenticateToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET']), rkbController.updateStatus);
+router.get('/', verifyToken, rkbController.getAllRKBs);
+router.get('/:id', verifyToken, rkbController.getRKBById);
+router.post('/', verifyToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET', 'ADMIN_UNIT']), rkbController.createRKB);
+router.post('/:id/items', verifyToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET', 'ADMIN_UNIT']), rkbController.addItem);
+router.put('/:id/status', verifyToken, authorizeRole(['SUPER_ADMIN', 'ADMIN_ASET']), rkbController.updateStatus);
 
 module.exports = router;
