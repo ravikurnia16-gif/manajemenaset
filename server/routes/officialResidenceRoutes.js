800823d2d9ea559378128b3d033ac6ce5e988a30
ravikurnia16-gif <ravikurnia16@gmail.com>
2026-03-11 09:57:32 +0700

    service

--- server/routes/officialResidenceRoutes.js
+++ server/routes/officialResidenceRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const officialResidenceController = require('../controllers/officialResidenceController');
-const { protect } = require('../middlewares/authMiddleware');
+const { verifyToken } = require('../middleware/authMiddleware');
 
 // Middleware to protect all routes
-router.use(protect);
+router.use(verifyToken);
 
 // Dashboard
 router.get('/dashboard', officialResidenceController.getDashboardStats);
