5a2de44b217f79fb0c42a89f5df68ff4796bc036
ravikurnia16-gif <ravikurnia16@gmail.com>
2026-04-20 10:34:44 +0700

    fix: remove Sarana dan Prasarana from KPI filter as requested

--- server/controllers/personnelController.js
+++ server/controllers/personnelController.js
@@ -1400,7 +1400,6 @@ exports.getKPILeaderboard = async (req, res) => {
                         { position: { contains: 'Staff Keuangan dan Administrasi' } },
                         { position: { contains: 'Staf Gudang dan Logistik' } },
                         { position: { contains: 'Staf Kendaraan' } },
-                        { position: { contains: 'Sarana dan Prasarana' } },
                     ]
                 },
                 {
