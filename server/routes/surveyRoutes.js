648e90d07d62861917c52ca27e666d532f23a660
ravikurnia16 <ravikurnia16@gmail.com>
2026-06-08 10:10:13 +0700

    fix: correct middleware path and function name in surveyRoutes

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const surveyController = require('../controllers/surveyController');
-const { verifyToken, authorizeRoles } = require('../middlewares/authMiddleware');
+const { verifyToken, authorizeRole } = require('../middleware/authMiddleware');
 
 // PUBLIC ROUTES
 router.get('/active-questions', surveyController.getActiveQuestions);
@@ -10,12 +10,12 @@ router.post('/submit', surveyController.submitSurvey);
 // ADMIN ROUTES (Requires Login & Role)
 const adminRoles = ['SUPER_ADMIN', 'ADMIN_ASET', 'KABID_SARPRAS'];
 
-router.get('/stats', verifyToken, authorizeRoles(...adminRoles), surveyController.getSurveyStats);
-router.get('/responses', verifyToken, authorizeRoles(...adminRoles), surveyController.getSurveyResponses);
+router.get('/stats', verifyToken, authorizeRole(adminRoles), surveyController.getSurveyStats);
+router.get('/responses', verifyToken, authorizeRole(adminRoles), surveyController.getSurveyResponses);
 
-router.get('/questions', verifyToken, authorizeRoles(...adminRoles), surveyController.getQuestionsAdmin);
-router.post('/questions', verifyToken, authorizeRoles(...adminRoles), surveyController.createQuestion);
-router.put('/questions/:id', verifyToken, authorizeRoles(...adminRoles), surveyController.updateQuestion);
-router.delete('/questions/:id', verifyToken, authorizeRoles(...adminRoles), surveyController.deleteQuestion);
+router.get('/questions', verifyToken, authorizeRole(adminRoles), surveyController.getQuestionsAdmin);
+router.post('/questions', verifyToken, authorizeRole(adminRoles), surveyController.createQuestion);
+router.put('/questions/:id', verifyToken, authorizeRole(adminRoles), surveyController.updateQuestion);
+router.delete('/questions/:id', verifyToken, authorizeRole(adminRoles), surveyController.deleteQuestion);
 
 module.exports = router;
